4b0076f98ecf39c180b0ce95e23e4c866befff8e
Luis Eduardo Cordeiro Salarini <eduardo.pmnf@gmail.com>
2022-05-02 14:42:53 -0300

    mudanças

--- settings/settings.py
+++ settings/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': db_name,
+        'PORT': '',
+
+        'USER': db_user,
+        'PASSWORD': db_passwd,
+        'HOST': '127.0.0.1',
     }
 }
 
